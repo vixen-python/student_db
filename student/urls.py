2464526c97304261502d439f864d14749a2afa0c
Dominika Pupakova <dominika.pupakova@localhost>
2021-07-18 11:51:56 +0200

    Login + Applications

---
+++ student/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from student.views import student_info_by_id, contact_info_by_id, address_info_by_id, \
+    StudentCreateView, ContactCreateView, StudentUpdateView, ContactUpdateView, StudentDeleteView
+
+app_name = 'student'
+
+urlpatterns = [
+    path('<int:requested_id>/', student_info_by_id, name='student_card'),
+    path('contacts/<int:requested_id>', contact_info_by_id, name='contact_card'),
+    path('address/<int:requested_id>', address_info_by_id, name='address_card'),
+    path('create/', StudentCreateView.as_view(), name='student_create'),
+    path('contact/create/', ContactCreateView.as_view(), name='contact_create'),
+    path('update/<pk>/', StudentUpdateView.as_view(), name='student_update'),
+    path('contact/update/<pk>/', ContactUpdateView.as_view(), name='contact_update'),
+    path('delete/<pk>/', StudentDeleteView.as_view(), name='student_delete'),
+]
