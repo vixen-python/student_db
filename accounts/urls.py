30e23a2e99e8711a08df3504fda3565ab511dfd4
Dominika Pupakova <dominika.pupakova@localhost>
2021-07-24 13:19:28 +0200

    Sign In

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accounts.views import SubmittableLogoutView
+from accounts.views import SubmittableLogoutView, SignUpView, MeView
 
 app_name = 'accounts'
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('logout/request/', SubmittableLogoutView.as_view(), name='logout_request')
+    path('logout/request/', SubmittableLogoutView.as_view(), name='logout_request'),
+    path('sign_up/', SignUpView.as_view(), name='sign_up'),
+    path('me/', MeView.as_view(), name='me')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,32 @@
-from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView, UpdateView
+
+from accounts.forms import SignUpForm, MeForm
 
 
 class SubmittableLogoutView(TemplateView):
     template_name = 'registration/logged_out.html'
+
+
+class SignUpView(CreateView):
+    template_name = "form.html"
+    form_class = SignUpForm
+    success_url = reverse_lazy('index')
+
+
+class MeView(LoginRequiredMixin, UpdateView):
+    template_name = 'form.html'
+    model = User
+    form_class = MeForm
+    success_url = reverse_lazy('accounts:me')
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def get_initial(self):
+        result = super().get_initial()
+        result['personal_phone'] = self.request.user.profile.personal_phone
+        result['permanent_address'] = self.request.user.profile.permanent_address
+        return result
